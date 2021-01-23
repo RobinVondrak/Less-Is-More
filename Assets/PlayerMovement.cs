4e7fecb5dd45d31229f4b02f5325ac49abe5f67a
SeasterSwe <Jakobbulow01@gmail.com>
2021-01-23 17:43:16 +0100

    Brap

---
+++ Assets/PlayerMovement.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private float walkSpeed = 5;
+    [SerializeField] private float runSpeed = 8;
+    float currentSpeed;
+    float speedSmoothVelocity;
+    Rigidbody2D rb;
+
+    void Awake()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        float x = Input.GetAxisRaw("Horizontal");
+        Vector3 input = new Vector3(x, 0, 0);
+
+        bool running = Input.GetKey(KeyCode.LeftShift);
+        float targetSpeed = ((running) ? runSpeed : walkSpeed) * input.magnitude;
+        
+        currentSpeed = Mathf.SmoothDamp(currentSpeed, targetSpeed, ref speedSmoothVelocity, 0.3f);
+        
+        Vector3 force = input * currentSpeed;
+        rb.velocity = new Vector2(force.x, rb.velocity.y);
+    }
+    private void FixedUpdate()
+    {
+        
+    }
+}
