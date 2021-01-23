e1b10124e6ec2f5bfbf019556a0daf06488e3117
RobinV <robinvondrak01@gmail.com>
2021-01-23 18:22:00 +0100

    Out of bounds

---
+++ Assets/Scripts/MovementAddForce.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MovementAddForce : MonoBehaviour
+{
+    [SerializeField] private float walkSpeed = 5;
+    [SerializeField] private float runSpeed = 8;
+    [SerializeField] private float movementSpeed = 200f;
+    private float maxSpeed = 200f;
+    private float currentSpeed;
+    private float speedSmoothVelocity;
+    Rigidbody2D rBody;
+
+    void Awake()
+    {
+        rBody = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        Vector2 input = new Vector2(Input.GetAxis("Horizontal"), 0);
+        rBody.AddForce(input * movementSpeed);
+
+        if (rBody.velocity.magnitude > maxSpeed)
+            rBody.velocity = Vector2.ClampMagnitude(rBody.velocity, maxSpeed);
+    }
+}
---
+++ Assets/Scripts/OutOfBounds.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+
+public class OutOfBounds : MonoBehaviour
+{
+    public Transform player;
+    public Transform resetPos;
+    public Transform minX, maxX, minY, maxY;
+
+    private void Update()
+    {
+        if (player.position.x < minX.position.x ||
+            player.position.x > maxX.position.x ||
+            player.position.y < minY.position.y ||
+            player.position.y > maxY.position.y)
+        {
+            player.position = resetPos.position;
+            Debug.Log("Out of bounds");
+        }
+    }
+}
