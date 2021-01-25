50fc1f44127a3c0550143cccc9653678e5c18d02
SeasterSwe <Jakobbulow01@gmail.com>
2021-01-25 11:32:43 +0100

    Art & Dialogs :D

---
+++ Assets/HappyBrun.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class HappyBrun : MonoBehaviour
+{
+    public TextMeshPro text;
+    GameObject player;
+    public string[] sentences;
+    int n;
+    bool enterd = false;
+    void Start()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+
+    void Update()
+    {
+        if (enterd && Input.GetKeyDown(KeyCode.N))
+            NextText();
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject != player)
+            return;
+
+        enterd = true;
+        NextText();
+    }
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.gameObject == player)
+            enterd = false;
+    }
+
+    void NextText()
+    {
+        text.text = sentences[n];
+        n++;
+    }
+}
