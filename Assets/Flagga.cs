337683bcfbe85ad2058b4ce8854bd28a5f9d09a4
SeasterSwe <Jakobbulow01@gmail.com>
2021-01-25 18:31:46 +0100

    Dotween ochSlimes

---
+++ Assets/Flagga.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class Flagga : MonoBehaviour
+{
+    public GameObject stars;
+    bool hissad = false;
+    void HissaFlagga()
+    {
+        if (!hissad)
+        {
+            hissad = true; //disabla collider ist?
+            Transform flagga = transform.Find("FlaggaFlagg");
+            Transform flaggaPos = transform.Find("TopOfFlaggaPos");
+            flagga.DOMove(flaggaPos.position, 1f).OnComplete(SkapaFlagga);
+        }
+    }
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+            HissaFlagga();
+    }
+    void SkapaFlagga()
+    {
+        Transform flaggaPos = transform.Find("TopOfFlaggaPos");
+        GameObject star = Instantiate(stars, flaggaPos.transform.position + (Vector3.up * 0.8f), stars.transform.rotation);
+    }
+}
---
+++ Assets/SimpleSlime.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+
+public class SimpleSlime : MonoBehaviour
+{
+    Transform slimeObj;
+    Vector3[] positions;
+    int posIndex = 0;
+    bool goingLeft = false;
+    private void Start()
+    {
+        slimeObj = transform.Find("SlimeObj");
+        positions = new Vector3[transform.childCount - 1];
+        int n = 0;
+        foreach (Transform g in GetComponentsInChildren<Transform>())
+        {
+            if (g.name == "Pos")
+            {
+                positions[n] = g.position;
+                n++;
+            }
+        }
+        slimeObj.position = positions[0];
+        NextPoint();
+    }
+    void NextPoint()
+    {
+        slimeObj.transform.DOMove(positions[posIndex], 0.2f).OnComplete(NextPos).SetEase(Ease.Linear);
+    }
+    void NextPos()
+    {
+        if (goingLeft)
+        {
+            posIndex -= 1;
+            if (posIndex <= 0)
+            {
+                posIndex = 0;
+                goingLeft = false;
+            }
+        }
+        else
+        {
+            posIndex += 1;
+            if (posIndex >= positions.Length - 1)
+            {
+                posIndex = positions.Length - 1;
+                goingLeft = true;
+            }
+        }
+        NextPoint();
+    }
+}
